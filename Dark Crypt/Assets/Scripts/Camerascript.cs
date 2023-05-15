a46a472de22abea5e3fd87293c319690e776220a
Miquel Nadal <miquelnadalmudarra@gmail.com>
2023-05-15 20:28:28 +0200

    Script de la camera + Import de Texturas

---
+++ Dark Crypt/Assets/Scripts/Camerascript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerascript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    private float targetDistance;
+
+    [SerializeField]
+    private float cameraLerp;
+
+
+    float cameraMaxConstraint = 70f;
+    float cameraMinConstraint = 30f;
+
+    private float rotationX;
+    private float rotationY;
+
+    private void LateUpdate()
+    {
+        rotationX += Input.GetAxis("Mouse Y");
+        rotationY += Input.GetAxis("Mouse X");
+
+        rotationX = Mathf.Clamp(rotationX, cameraMinConstraint, cameraMaxConstraint);
+
+
+        transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
+
+        transform.position = Vector3.Lerp(transform.position, target.transform.position - transform.forward * targetDistance, cameraLerp * Time.deltaTime);
+    }
+}
