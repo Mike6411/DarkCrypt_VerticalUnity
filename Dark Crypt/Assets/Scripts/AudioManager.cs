6ba005148a820b1b4717af60915a4aeafb39819b
Miquel Nadal <miquelnadalmudarra@gmail.com>
2023-05-16 23:36:32 +0200

    AudioManager Basico y metiendo los sonidos

---
+++ Dark Crypt/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioSource ambientMusic;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else { Destroy(gameObject); }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+
+}
